50f87738de031b384c99a06660c271df280f848a
WD4715 <117700793+WD4715@users.noreply.github.com>
2023-06-15 22:38:38 +0900

    Add files via upload

---
+++ Triangulation/build/CMakeFiles/Triangulartion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Triangulartion.dir/trianglation.cpp.o"
+  "Triangulartion"
+  "Triangulartion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Triangulartion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
