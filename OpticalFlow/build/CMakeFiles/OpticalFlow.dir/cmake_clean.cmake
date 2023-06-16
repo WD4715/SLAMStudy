6ca220bb7da393486125b71dc817eaf092bed00e
WD4715 <117700793+WD4715@users.noreply.github.com>
2023-06-16 11:05:25 +0900

    Optical flow using opencv and Optical flow using Custom Gauss-Newton Algorithm method.

---
+++ OpticalFlow/build/CMakeFiles/OpticalFlow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpticalFlow.dir/OpticalFlow.cpp.o"
+  "OpticalFlow"
+  "OpticalFlow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpticalFlow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
