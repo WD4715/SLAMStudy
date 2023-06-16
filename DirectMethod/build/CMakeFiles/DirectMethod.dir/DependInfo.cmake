a677d9c0bd29d4f5252e64de3a7b386d4c6e55a9
WD4715 <117700793+WD4715@users.noreply.github.com>
2023-06-16 17:33:11 +0900

    Add files via upload
    This is C++ implementation of Direct method.

---
+++ DirectMethod/build/CMakeFiles/DirectMethod.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/JLK/Desktop/Robo/DirectMethod/DirectMethod.cpp" "/mnt/c/Users/JLK/Desktop/Robo/DirectMethod/build/CMakeFiles/DirectMethod.dir/DirectMethod.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "PANGO_DEFAULT_WIN_URI=\"wayland\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  "/usr/include/eigen3"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_core/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_display/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_opengl/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_image/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_windowing/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/build/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_vars/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_geometry/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/tinyobj/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_glgeometry/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_packetstream/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_plot/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_scene/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_tools/include"
+  "/mnt/c/Users/JLK/Desktop/Robo/thridparty/Pangolin/components/pango_video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ DirectMethod/build/CMakeFiles/DirectMethod.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DirectMethod.dir/DirectMethod.cpp.o"
+  "DirectMethod"
+  "DirectMethod.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DirectMethod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
