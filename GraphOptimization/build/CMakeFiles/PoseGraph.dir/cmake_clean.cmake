a501db4a4b0c95da8317a71aac2a2a93d100d26e
WD4715 <117700793+WD4715@users.noreply.github.com>
2023-06-21 11:57:06 +0900

    Add files via upload

---
+++ GraphOptimization/build/CMakeFiles/PoseGraph.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PoseGraph.dir/pose_graph_g2o_SE3.cpp.o"
+  "PoseGraph"
+  "PoseGraph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PoseGraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
